05727446ff1d1947ef63ba437a171e1e82153fd1
Murat Ilgun <muratilgun34@hotmail.com>
2020-11-25 18:40:30 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities.Concrete;
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün başarıyla eklendi";
+        public static string ProductDeleteded = "Ürün başarıyla silindi";
+        public static string ProductUpdateded = "Ürün başarıyla güncellendi";
+
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre Hatalı";
+
+        public static string SuccessfulLogin = "Sisteme Giriş Başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi.";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu.";
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfUserDal : EfEntityRepositoryBase<User,NorthwindContext>, IUserDal
+    {
+        public List<OperationClaim> GetClaims(User user)
+        {
+            using (var context = new NorthwindContext())
+            {
+                var result = from operationclaim in context.OperationClaims
+                    join userOperationClaim in context.UserOperationClaims
+                        on operationclaim.Id equals userOperationClaim.OperationClaimId
+                    where userOperationClaim.UserId == user.Id
+                    select new OperationClaim {Id = operationclaim.Id, Name = operationclaim.Name};
+                    return result.ToList();
+
+
+            }
+        }
+    }
+}
